95467edbd1f2dc446d22201b4ee258d786614c64
Robert Sundaye <111018257+robertsundaye@users.noreply.github.com>
2024-01-07 10:58:03 +0000

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/pytho3
 
-def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific position in a list."""
+def delete_at(my_list=[],idx=0):
+    """delete an item at a specific position in a lis
+t"""
     if idx < 0 or idx >= len(my_list):
-       return my_list
+        return my_list
     else:
-         del(my_list[idx])
-         return my_list
+        del(my_list[idx])
+        return my_list
